986f482b486829b21c071fb8a8b5369760f9c651
D20CM <scottbrown500@hotmail.com>
2021-11-12 12:10:51 +0000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+
+
+// var anneBox = document.getElementById("anne-box");
+
+
+
+// var onButtonClick = function() {
+//     if(anneBox.style.transform = "scale(1.2)"){
+//     anneBox.style.transform = "scale(1.8)";
+//     }
+    
+//     anneBox.style.transform = "1.8";
+    
+// };
+
+// anneBox.addEventListener("click",onButtonClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("anneImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
