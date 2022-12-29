b43aa0334746388214aa77948c2b293590beb83f
D20CM <scottbrown500@hotmail.com>
2022-12-29 19:18:19 +0000

    add hamburger menu on dummy page, tweak css

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,24 @@
+let burgerLinks = document.getElementById("menuLinks");
+let burgerMenu = document.getElementById("burger-menu");
+let closeBurger = document.getElementById("closeBurger");
+let burgerIcon = document.getElementById("burgerIcon");
 
+burgerMenu.addEventListener("click", toggleBurger);
 
-// var anneBox = document.getElementById("anne-box");
-
-
-
-// var onButtonClick = function() {
-//     if(anneBox.style.transform = "scale(1.2)"){
-//     anneBox.style.transform = "scale(1.8)";
-//     }
-    
-//     anneBox.style.transform = "1.8";
-    
-// };
-
-// anneBox.addEventListener("click",onButtonClick);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById("anneImg");
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+function toggleBurger() {
+  if (!burgerLinks.classList.contains("open")) {
+    burgerLinks.classList.remove("closed");
+    burgerLinks.classList.add("open");
+    burgerIcon.classList.remove("open");
+    burgerIcon.classList.add("closed");
+    closeBurger.classList.remove("closed");
+    closeBurger.classList.add("open");
+  } else {
+    burgerLinks.classList.remove("open");
+    burgerLinks.classList.add("closed");
+    burgerIcon.classList.remove("closed");
+    burgerIcon.classList.add("open");
+    closeBurger.classList.remove("open");
+    closeBurger.classList.add("closed");
+  }
+}
