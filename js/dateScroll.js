1c1efcd4686b6e9a8b0afc69c7721b8aad00bb31
D20CM <scottbrown500@hotmail.com>
2023-07-23 16:55:30 +0100

    date-scroll feature working

--- js/dateScroll.js
+++ js/dateScroll.js
@@ -16,9 +16,21 @@ console.log(new Date(comparisonDate));
 
 // let targetDate = timeDifferences[0];
 
-let sortedGigsList = gigsList.sort(
-  (a, b) => a - comparisonDate - (b - comparisonDate)
-);
+let sortedGigsList = gigsList.sort((a, b) => {
+  console.log("Comparison date: ", comparisonDate);
+  console.log("a: ", Date.parse(a.getAttribute("data-date")));
+  console.log("b: ", Date.parse(b.getAttribute("data-date")));
+  if (
+    Date.parse(a.getAttribute("data-date")) - comparisonDate <
+    Date.parse(b.getAttribute("data-date")) - comparisonDate
+  )
+    return -1;
+  else if (
+    Date.parse(a.getAttribute("data-date")) - comparisonDate >
+    Date.parse(b.getAttribute("data-date")) - comparisonDate
+  )
+    return 1;
+});
 
 console.log("Sorted gigs list: ", sortedGigsList);
 
